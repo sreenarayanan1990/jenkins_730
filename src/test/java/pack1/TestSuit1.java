aac480cabf780a2d431bbd59ae2865ff9e45279c
SREENARAYANAN <sreenarayanan@example.com>
2025-05-13 14:17:19 +0530

    update

---
+++ src/test/java/pack1/TestSuit1.java
@@ -0,0 +1,26 @@
+package pack1;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestSuit1 {
+
+	@Test
+	public void test() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		if (driver.getTitle().equals("Google")) {
+			System.out.println("pass");
+		} else {
+			System.out.println("fail");
+		}
+		driver.quit();
+
+	}
+
+}
